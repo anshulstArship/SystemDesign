6323b972fdd354dfebd95eb88f5f8623a3199e3d
Anshul <anshul.tyagi306@gmail.com>
2025-06-14 18:15:41 +0530

    Added Basic Structure for Video Sharing Platform

---
+++ StreamBase/src/test/java/com/main/streambase/StreamBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.streambase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
